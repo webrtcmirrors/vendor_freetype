c60dd2d5aaf6e01e3898e044945253e6de010cc7
Ewald Hew <ewaldhew@gmail.com>
2017-05-20 17:58:28 +0800

    Added func ptrs for cff get/free glyph_data (callback)

--- src/cff/cffparse.c
+++ src/cff/cffparse.c
@@ -20,10 +20,10 @@
 #include "cffparse.h"
 #include FT_INTERNAL_STREAM_H
 #include FT_INTERNAL_DEBUG_H
+#include FT_INTERNAL_POSTSCRIPT_AUX_H
 
 #include "cfferrs.h"
 #include "cffpic.h"
-#include "cffgload.h"
 #include "cffload.h"
 
 
@@ -1279,6 +1279,7 @@
     FT_Library  library = parser->library;
     FT_UNUSED( library );
 
+    
 
     parser->top    = parser->stack;
     parser->start  = start;
@@ -1363,10 +1364,14 @@
         cff_rec.top_font.font_dict.num_axes    = parser->num_axes;
         decoder.cff                            = &cff_rec;
 
-        error = cff_decoder_parse_charstrings( &decoder,
-                                               charstring_base,
-                                               charstring_len,
-                                               1 );
+        /* TODO(ewaldhew): link this
+        PSAux_Service            psaux         = cff->psaux;
+        const CFF_Decoder_Funcs  decoder_funcs = psaux->cff_decoder_funcs;
+        */
+        error = decoder_funcs->parse_charstrings( &decoder,
+                                                  charstring_base,
+                                                  charstring_len,
+                                                  1 );
 
         /* Now copy the stack data in the temporary decoder object,    */
         /* converting it back to charstring number representations     */
